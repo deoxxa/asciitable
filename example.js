0eac1bcdc18c371d788926c581f74b31cd03aa1f
Niggler <foo@bar.org>
2013-03-06 16:12:27 -0500

    fixed issue with title widths
    Fixes #1
    
    Also updated example to show effect

--- example.js
+++ example.js
@@ -3,12 +3,12 @@
 var asciitable = require("./");
 
 var table = asciitable([
-  {a: "a", b: "b", c: "c"},
-  {a: "asddsfa", b: "sss", c: "zxc"},
-  {a: "dsgvdgsdvgssdf", b: "x", c: "eryty"},
-  {a: "m", b: "n", c: "o", d: "p"},
-  {a: 5, b: null, c: {}, d: []},
-  {a: function(){}},
+  {ab: "a", b: "b", c: "c"},
+  {ab: "asddsfa", b: "sss", c: "zxc"},
+  {ab: "dsgvdgsdvgssdf", b: "x", c: "eryty"},
+  {ab: "m", b: "n", c: "o", de: "p"},
+  {ab: 5, b: null, c: {}, de: []},
+  {ab: function(){}},
 ]);
 
 console.log(table);
